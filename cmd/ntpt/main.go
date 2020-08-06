d3fdb0de3ff745f348c1247946ad0f8c5926ab88
Adam Chalkley <atc0005@users.noreply.github.com>
2020-08-06 05:18:34 -0500

    Add Makefile, linting config, branding details
    - Add Makefile to drive local builds, linting
    - Add golangci-lint config file for local linting
    - Borrow code from atc0005/dnsc to provide branding
      details when an invalid option is provided (or none at all)
      or the help text is requested
    
    refs GH-5

--- cmd/ntpt/main.go
+++ cmd/ntpt/main.go
@@ -11,36 +11,72 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"time"
 
 	"github.com/beevik/ntp"
 )
 
+// Overridden via Makefile for release builds
+var version string = "dev build"
+
+// Primarily used with branding
+const myAppName string = "ntpt"
+const myAppURL string = "https://github.com/atc0005/" + myAppName
+
+const (
+	// defaultNTPServer is intentionally set to an empty string. Since our
+	// intent is to test a specific server, we don't want to hard-code a
+	// default NTP server.
+	defaultNTPServer string = ""
+)
+
+const (
+	// ntpServerFlagHelp is the help text provided to the user for that
+	// flag option
+	ntpServerFlagHelp string = "NTP time server to query"
+)
+
+// Branding is responsible for emitting application name, version and origin
+func Branding() {
+	fmt.Fprintf(
+		flag.CommandLine.Output(),
+		"\n%s %s\n%s\n\n",
+		myAppName,
+		version,
+		myAppURL,
+	)
+}
+
+// flagsUsage displays branding information and general usage details
+func flagsUsage() func() {
+
+	return func() {
+
+		myBinaryName := filepath.Base(os.Args[0])
+
+		Branding()
+
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of \"%s\":\n",
+			myBinaryName,
+		)
+		flag.PrintDefaults()
+
+	}
+}
+
 func main() {
-
-	const (
-		// defaultNTPServer is intentionally set to an empty string. Since our
-		// intent is to test a specific server, we don't want to hard-code a
-		// default NTP server.
-		defaultNTPServer string = ""
-	)
-
-	const (
-		// ntpServerFlagHelp is the help text provided to the user for that
-		// flag option
-		ntpServerFlagHelp string = "NTP time server to query"
-	)
 
 	var ntpServer string
 
 	flag.StringVar(&ntpServer, "server", defaultNTPServer, ntpServerFlagHelp)
 	flag.StringVar(&ntpServer, "s", defaultNTPServer, ntpServerFlagHelp+" (shorthand)")
+	flag.Usage = flagsUsage()
 	flag.Parse()
 
 	if ntpServer == "" {
 		fmt.Println("NTP server not specified!")
-		fmt.Printf("\nUsage:\n")
-		flag.PrintDefaults()
+		flag.Usage()
 		os.Exit(1)
 	}
 
