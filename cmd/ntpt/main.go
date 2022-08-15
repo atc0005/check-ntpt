19f93e7fde7502df14bdb5851581920fdf928a73
Adam Chalkley <atc0005@users.noreply.github.com>
2022-08-15 06:18:20 -0500

    Add verbose flag, simplify default output
    - add `-v`, `--verbose` flags
    - update README coverage
    - use simply config struct to hold CLI flag settings
    - simplify default output
    - place verbose output behind conditional check
    
    refs GH-84

--- cmd/ntpt/main.go
+++ cmd/ntpt/main.go
@@ -24,18 +24,26 @@ var version = "dev build"
 const myAppName string = "ntpt"
 const myAppURL string = "https://github.com/atc0005/" + myAppName
 
+// Default flag settings if not overridden by user input.
 const (
 	// defaultNTPServer is intentionally set to an empty string. Since our
 	// intent is to test a specific server, we don't want to hard-code a
 	// default NTP server.
 	defaultNTPServer string = ""
+
+	defaultVerboseOutput bool = false
 )
 
+// Flag help text (user visible).
 const (
-	// ntpServerFlagHelp is the help text provided to the user for that
-	// flag option
-	ntpServerFlagHelp string = "NTP time server to query"
+	ntpServerFlagHelp     string = "NTP time server to query"
+	verboseOutputFlagHelp string = "Toggles emission of detailed certificate metadata. This level of output is disabled by default."
 )
+
+type config struct {
+	server  string
+	verbose bool
+}
 
 // Branding is responsible for emitting application name, version and origin
 func Branding() {
@@ -67,62 +75,94 @@ func flagsUsage() func() {
 
 func main() {
 
-	var ntpServer string
+	var cfg config
 
-	flag.StringVar(&ntpServer, "server", defaultNTPServer, ntpServerFlagHelp)
-	flag.StringVar(&ntpServer, "s", defaultNTPServer, ntpServerFlagHelp+" (shorthand)")
+	flag.StringVar(&cfg.server, "server", defaultNTPServer, ntpServerFlagHelp)
+	flag.StringVar(&cfg.server, "s", defaultNTPServer, ntpServerFlagHelp+" (shorthand)")
+	flag.BoolVar(&cfg.verbose, "verbose", defaultVerboseOutput, verboseOutputFlagHelp)
+	flag.BoolVar(&cfg.verbose, "v", defaultVerboseOutput, verboseOutputFlagHelp+" (shorthand)")
 	flag.Usage = flagsUsage()
 	flag.Parse()
 
-	if ntpServer == "" {
+	if cfg.server == "" {
 		fmt.Println("NTP server not specified!")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	ntpServerTime, err := ntp.Time(ntpServer)
+	ntpServerTime, err := ntp.Time(cfg.server)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Current time from %s: %+v\n", ntpServer, ntpServerTime)
+	localTime := time.Now()
+
+	fmt.Printf("Current time from %s: %v\n", cfg.server, ntpServerTime)
+	fmt.Printf("Current time from local system: %v\n", localTime)
 
 	// options := ntp.QueryOptions{Timeout: 30 * time.Second, TTL: 5}
 	// response, err := ntp.QueryWithOptions(ntpServer, options)
-	response, err := ntp.Query(ntpServer)
+	response, err := ntp.Query(cfg.server)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf(
-		"Response from NTP server %q: \n"+
-			"\tTime: %v\n"+
-			"\tClockOffset: %v\n"+
-			"\tRTT: %v\n"+
-			"\tStratum: %v\n"+
-			"\tReferenceID: %v\n"+
-			"\tReferenceTime: %v\n"+
-			"\tRootDelay: %v\n"+
-			"\tRootDispersion: %v\n"+
-			"\tRootDistance: %v\n"+
-			"\tLeap: %v\n"+
-			"\tMinError: %v\n"+
-			"\tKissCode: %q\n",
-		ntpServer,
-		response.Time,
-		response.ClockOffset,
-		response.RTT,
-		response.Stratum,
-		response.ReferenceID,
-		response.ReferenceTime,
-		response.RootDelay,
-		response.RootDispersion,
-		response.RootDistance,
-		response.Leap,
-		response.MinError,
-		response.KissCode,
-	)
 
-	offsetAdjTime := time.Now().Add(response.ClockOffset)
-	fmt.Println("Offset adjusted time:", offsetAdjTime)
+	// fmt.Printf(
+	// 	"Estimated offset of local client clock relative to %s: %+v\n",
+	// 	cfg.server,
+	// 	response.ClockOffset,
+	// )
+
+	switch {
+	case cfg.verbose:
+		fmt.Printf(
+			"\nResponse from NTP server %q: \n"+
+				"\tTime: %v\n"+
+				"\tClockOffset: %v\n"+
+				"\tRTT: %v\n"+
+				"\tStratum: %v\n"+
+				"\tReferenceID: %v\n"+
+				"\tReferenceTime: %v\n"+
+				"\tRootDelay: %v\n"+
+				"\tRootDispersion: %v\n"+
+				"\tRootDistance: %v\n"+
+				"\tLeap: %v\n"+
+				"\tMinError: %v\n"+
+				"\tKissCode: %q\n",
+			cfg.server,
+			response.Time,
+			response.ClockOffset,
+			response.RTT,
+			response.Stratum,
+			response.ReferenceID,
+			response.ReferenceTime,
+			response.RootDelay,
+			response.RootDispersion,
+			response.RootDistance,
+			response.Leap,
+			response.MinError,
+			response.KissCode,
+		)
+
+		offsetAdjTime := time.Now().Add(response.ClockOffset)
+		fmt.Printf("\nOffset adjusted time: %v\n", offsetAdjTime)
+
+	default:
+
+		timeDirection := "behind"
+		if localTime.Before(ntpServerTime) {
+			timeDirection = "ahead of"
+		}
+
+		// The local system is 2m28.258534398s behind pool.ntp.org.
+		// The local system is 2m28.258534398s ahead of pool.ntp.org.
+		fmt.Printf(
+			"\nThe local system is %v %s %s.\n",
+			response.ClockOffset,
+			timeDirection,
+			cfg.server,
+		)
+
+	}
 
 }
