ef2fe6ee8db931106255e1b067011cfdfa32bc0c
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-03 06:56:26 -0500

    Update project to Go 1.19
    - update go.mod file from Go 1.17 to 1.19
    - update doc.go file to use Go 1.19 package doc comments syntax
    - update "Canary" Dockerfile to reflect current Go 1.19 version
    - update Dependabot configuration for Dockerfile to ignore Go
      releases outside of the Go 1.19 release series

--- doc.go
+++ doc.go
@@ -1,26 +1,23 @@
 /*
-
 Network Time Protocol (NTP) client for testing purposes.
 
-PROJECT HOME
+# Project Home
 
-See our GitHub repo (https://github.com/atc0005/ntpt) for the latest
-code, to file an issue or submit improvements for review and potential
-inclusion into the project.
+See our GitHub repo (https://github.com/atc0005/ntpt) for the latest code, to
+file an issue or submit improvements for review and potential inclusion into
+the project.
 
-PURPOSE
+# Purpose
 
 Perform a NTP query against a specified server for testing purposes.
 
-FEATURES
+# Features
 
-• single binary, no outside dependencies
+  - single binary, no outside dependencies
+  - query specified NTP server for current time
 
-• query specified NTP server for current time
-
-USAGE
+# Usage
 
 See our main README for supported settings and examples.
-
 */
 package main
