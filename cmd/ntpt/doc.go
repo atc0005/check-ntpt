88731fc305e8b2e9a12d5bcb63cab46dadb6e32d
Adam Chalkley <atc0005@users.noreply.github.com>
2022-09-03 07:00:25 -0500

    Add missing cmd doc file
    Add "package" doc file to resolve revive `package-comment`
    linting error surfaced by recent linter update.

---
+++ cmd/ntpt/doc.go
@@ -0,0 +1,18 @@
+// Copyright 2020 Adam Chalkley
+//
+// https://github.com/atc0005/ntpt
+//
+// Licensed under the MIT License. See LICENSE file in the project root for
+// full license information.
+
+// Perform a NTP query against a specified server for testing purposes.
+//
+// See our [GitHub repo]:
+//
+//   - to review documentation (including examples)
+//   - for the latest code
+//   - to file an issue or submit improvements for review and potential
+//     inclusion into the project
+//
+// [GitHub repo]: https://github.com/atc0005/ntpt
+package main
